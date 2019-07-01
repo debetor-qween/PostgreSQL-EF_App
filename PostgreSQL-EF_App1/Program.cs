361d0fc1a03e11badf59c251cc7c4b3bc1b200ae
Sergei <debetor@mail.ru>
2019-07-01 18:39:05 +0300

    Изменена последовательность операций. Теперь: 1) INSERT в Users 2) SELECT из Users 3) SELECT из Apartments

--- PostgreSQL-EF_App1/Program.cs
+++ PostgreSQL-EF_App1/Program.cs
@@ -10,32 +10,39 @@ namespace PostgreSQL_EF_App1
         {
             DateTime startTime, endTime;
             ApplicationContext db = new ApplicationContext();
-            DbSet<User> users = db.Users;
+
             startTime = DateTime.Now;
-            Console.WriteLine("Original Users list:");
-            foreach (User u in users)
-            {
-                Console.WriteLine($"Id = {u.Id}. Name:{u.Name}. Login: {u.Login}");
-            }
-            endTime = DateTime.Now;
-            Console.WriteLine($"Время выполнения запроса: {((endTime-startTime)).TotalMilliseconds}");
-
-            Console.ReadLine();
-
             User user1 = new User { Name = "Novice", Login = "NoviceLogin", Pwd = "NovicePwd" };
             db.Users.Add(user1);
             db.SaveChanges();
+            endTime = DateTime.Now;
+            Console.WriteLine($"Время выполнения INSERT: {((endTime - startTime)).TotalMilliseconds}");
 
+
+            DbSet<User> users = db.Users;
             startTime = DateTime.Now;
-            Console.WriteLine("Updated Users list:");
+            Console.WriteLine("\nUsers list:");
             foreach (User u in users)
             {
                 Console.WriteLine($"Id = {u.Id}. Name:{u.Name}. Login: {u.Login}");
             }
             endTime = DateTime.Now;
-            Console.WriteLine($"Время выполнения запроса: {((endTime - startTime)).TotalMilliseconds}");
+            Console.WriteLine($"Время выполнения запроса из таблицы User: {((endTime-startTime)).TotalMilliseconds}");
+
+            // Console.ReadLine();
+
+            DbSet<Apartment> apartments = db.Apartments;
+            startTime = DateTime.Now;
+            Console.WriteLine("\nOriginal Apartments list:");
+            foreach (Apartment a in apartments)
+            {
+                Console.WriteLine($"Id = {a.Id}. Name:{a.Name}. Parent: {(a.Parent_id)}");
+            }
+            endTime = DateTime.Now;
+            Console.WriteLine($"Время выполнения запроса из таблицы Apartment: {((endTime - startTime)).TotalMilliseconds}");
 
             Console.ReadLine();
+
 
         }
     }
@@ -43,6 +50,7 @@ namespace PostgreSQL_EF_App1
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Apartment> Apartments { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -58,4 +66,12 @@ namespace PostgreSQL_EF_App1
         public string Pwd { get; set; }
 
     }
+
+    public class Apartment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Parent_id { get; set; }
+    }
+
 }
